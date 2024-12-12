8e3e319d34424600ac72527b883c423f8344a937
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-12-12 12:38:50 -0500

    Ejercicios Hechos

--- EjerciciosT9/src/paquete02/Ejercicio04.java
+++ EjerciciosT9/src/paquete02/Ejercicio04.java
@@ -15,13 +15,31 @@ public class Ejercicio04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         double[] promedios = {10, 10, 9.1, 7, 6.1, 4, 8};
-        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan", 
+        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan",
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
         String[] promediosCualitativos = new String[7];
-        
-    }
+        String cualidad = "";
+        for (int i = 0; i < promediosCualitativos.length; i++) {
 
+            if (promedios[i] >= 0 && promedios[i] <= 5.9) {
+                cualidad = "Regular";
+            } else {
+                if (promedios[i] >= 6 && promedios[i] <= 8.9) {
+                    cualidad = "Buena";
+                } else {
+                  cualidad = "Sobresaliente";  
+                }
+            }
+                promediosCualitativos[i] = estudiantes[i] + " promedio: " + promedios[i] + " promedio cualitativo " + cualidad;
+
+            }
+        for (int i = 0; i < promediosCualitativos.length; i++){
+            System.out.println(promediosCualitativos[i]);
+        }
+        
+
+        }
 }

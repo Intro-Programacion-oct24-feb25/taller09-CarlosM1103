41f99ed0fc31b4536b4a57801328e533bae61d90
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-12-12 12:23:04 -0500

    Ejercicios Hechos

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -17,7 +17,28 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
+        double suma = 0;
+        double promedio = 0;
+        String arriba = "Numeros por encima del promedio:\n";
+        String abajo = "Numeros por debajo del promedio:\n";
         
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+        
+        promedio = suma/arreglo.length;
+        
+        for (int i = 0; i < arreglo.length; i++) {
+
+            if (arreglo[i] > promedio) {
+                arriba = String.format("%s%d\n", arriba, arreglo[i]);
+            } else {
+                abajo = String.format("%s%d\n", abajo, arreglo[i]);
+            }
+        }
+
+        System.out.printf("El promedio es:%.2f\n%s\n%s",promedio, arriba, abajo);
+
     }
 
 }
